2f90ec8a242e311f2267504d735a3e5e1c37cf08
Luis Edo Gonzalez O <luego1986@gmail.com>
2014-03-08 12:57:40 -0500

    Gems Installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlquilerMatrix::Application.config.secret_token = '3a0bad34f4a27eff9a456c409536064c518f7b50b9db271419abf4fd872a54d2f705e5bf5da78695992e734c5aa3431e4ad0619044a7a56f3fcd72d95648a44f'
