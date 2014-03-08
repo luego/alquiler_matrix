c939f681cf24327a985325a612f04e6f3474e9f6
Luis Edo Gonzalez O <luego1986@gmail.com>
2014-03-08 17:19:17 -0500

    units test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,9 @@ class Movie < ActiveRecord::Base
     square: '200x200#',
     medium: '300x300>'
   }
-
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :release, presence: true
+  validates :rate, presence: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Todos los campos son obligatorios" do
+    movie = Movie.new
+    movie.title = movies(:one).title
+
+    assert !movie.save
+  end
 end
